9a00af9ae1f45e76a22a5908f981dda5c6236161
rafaelvf <reviterif@gmail.com>
2023-01-24 18:50:06 -0500

    update al banner

--- delincuen_mapa/components/Nav.tsx
+++ delincuen_mapa/components/Nav.tsx
@@ -1,14 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Nav.module.scss";
 
+const dataCrimenes = ["5 Crimenes Registrados", "3 Muertes", "4 Desaparecidos"];
+
 const Nav = () => {
+  //const [crimenes, setCrimenes] = useState();
+
+  //   dataCrimenes.map((i: any) => {
+  //     setTimeout(() => {
+  //       setCrimenes(i);
+  //     }, 1000);
+  //   });
+
   return (
     <div className={styles.container}>
       <img src="/logod.png" className={styles.logo} />
-      <div className={styles.banner}>
-        <div className={styles.bannerText}>5 crimenes registrados</div>
+      <div className={styles.rightContainer}>
+        <div className={styles.banner}>
+          <div className={styles.bannerInside}>
+            {dataCrimenes.map((i: any, key: any) => (
+              <div className={styles.bannerText} key={key}>
+                {i}
+              </div>
+            ))}
+          </div>
+        </div>
+        <img src="/form-svgrepo-com.svg" className={styles.formButton}></img>
       </div>
-      <div className={styles.formButton}>ddddd</div>
     </div>
   );
 };
