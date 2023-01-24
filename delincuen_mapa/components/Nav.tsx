6531fdfdeffcb61a03d154984ee4de9114e88264
rafaelvf <reviterif@gmail.com>
2023-01-24 16:58:06 -0500

    new changes

---
+++ delincuen_mapa/components/Nav.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useRef } from "react";
+import styles from "../styles/Nav.module.scss";
+
+const Nav = () => {
+  return (
+    <div className={styles.container}>
+      <img src="/logod.png" className={styles.logo} />
+      <div className={styles.banner}>dddd</div>
+      <div className={styles.formButton}>ddddd</div>
+    </div>
+  );
+};
+export default Nav;
