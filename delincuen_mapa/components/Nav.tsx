19a1512c1a1446f04e57a19b6df4e8260b493d23
rafaelvf <reviterif@gmail.com>
2023-01-24 17:09:51 -0500

    banner update

--- delincuen_mapa/components/Nav.tsx
+++ delincuen_mapa/components/Nav.tsx
@@ -5,7 +5,9 @@ const Nav = () => {
   return (
     <div className={styles.container}>
       <img src="/logod.png" className={styles.logo} />
-      <div className={styles.banner}>dddd</div>
+      <div className={styles.banner}>
+        <div className={styles.bannerText}>5 crimenes registrados</div>
+      </div>
       <div className={styles.formButton}>ddddd</div>
     </div>
   );
