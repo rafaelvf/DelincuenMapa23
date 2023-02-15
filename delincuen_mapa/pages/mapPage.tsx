a28fa47cd20963d38729402cb0f990a839b7d23c
rafaelvf <reviterif@gmail.com>
2023-02-15 13:19:36 -0500

    update loading problems

--- delincuen_mapa/pages/mapPage.tsx
+++ delincuen_mapa/pages/mapPage.tsx
@@ -171,7 +171,11 @@ export default function mapPage() {
             <Map customers={customers2} />
           </div>
           <div className={styles.subSubContainer}>
-            <Board customers={customers2} handleClick={handleClick} />
+            {customers2.length === 0 ? (
+              <div>loading</div>
+            ) : (
+              <Board customers={customers2} handleClick={handleClick} />
+            )}
           </div>
         </div>
       </main>
