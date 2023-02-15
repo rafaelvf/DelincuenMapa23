37da9d8523cd14f9ddee6a411b539ee5a910a628
rafaelvf <reviterif@gmail.com>
2023-02-14 22:07:41 -0500

    white hola

--- delincuen_mapa/pages/index.tsx
+++ delincuen_mapa/pages/index.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         </div>
         <InfoCards />
         <Future />
-        <div>hola</div>
+        <div style={{ color: "white" }}>hola</div>
       </main>
 
       <footer></footer>
