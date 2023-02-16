4bc2cd3a0cb39ff60323a643ab28f1616d1d4495
rafaelvf <reviterif@gmail.com>
2023-02-16 12:35:36 -0500

    error

--- delincuen_mapa/components/Board.tsx
+++ delincuen_mapa/components/Board.tsx
@@ -5,27 +5,32 @@ import { useEffect, useState } from "react";
 import { clearFilter } from "../redux/userSlice";
 
 export default function Board({ customers, handleClick }: any) {
-  const arrayAmague: [] = customers || [];
+  const arrayAmague: [] = [] || customers;
   const customersFiltered = useSelector((state: any) => state.user.robos);
   console.log(customersFiltered, "customersFiltered2");
   console.log(customers, "customersFiltered23");
+  if (!customersFiltered) {
+    console.log("es flase");
+  }
   const peopleArray = !customersFiltered ? arrayAmague : customersFiltered;
   const dispatch = useDispatch();
+  console.log(typeof peopleArray);
   return (
     <div className={styles.container}>
-      {peopleArray.map((i: any, key: any) => (
-        <Tweets
-          //ciudad={i.ciudad}
-          fecha={i.fechaHora}
-          descripcion={i.descripcion}
-          tipo={i.tipo}
-          valor={i.valor}
-          articulo={i.articulo}
-          latitud={i.coordenadas.lat}
-          longitud={i.coordenadas.lng}
-          key={key}
-        />
-      ))}
+      {peopleArray &&
+        peopleArray.map((i: any, key: any) => (
+          <Tweets
+            //ciudad={i.ciudad}
+            fecha={i.fechaHora}
+            descripcion={i.descripcion}
+            tipo={i.tipo}
+            valor={i.valor}
+            articulo={i.articulo}
+            latitud={i.coordenadas.lat}
+            longitud={i.coordenadas.lng}
+            key={key}
+          />
+        ))}
       <div
         className={styles.borrarFiltros}
         onClick={() => {
