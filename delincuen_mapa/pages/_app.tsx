9711c58ed9932dbb9661461f28aededff57c76c2
rafaelvf <reviterif@gmail.com>
2023-02-16 17:23:29 -0500

    d

--- delincuen_mapa/pages/_app.tsx
+++ delincuen_mapa/pages/_app.tsx
@@ -2,10 +2,10 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../redux/store";
 import "../styles/global.css";
-import { updateCustomers } from "../redux/userSlice";
+//import { updateCustomers } from "../redux/userSlice";
 
 export default function App({ Component, pageProps }: AppProps) {
-  store.dispatch(updateCustomers());
+  //store.dispatch(updateCustomers());
   return (
     <Provider store={store}>
       <Component {...pageProps} />
